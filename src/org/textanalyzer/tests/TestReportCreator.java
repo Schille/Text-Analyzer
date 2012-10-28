a15598e8478b2a62ca6b068115bced418b3a18e7
Michael Schilonka <michael@schilonka.de>
2012-10-28 19:27:47 +0100

    added testcase for report creator

---
+++ src/org/textanalyzer/tests/TestReportCreator.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.textanalyzer.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.textanalyzer.reportcreator.ReportCreator;
+
+/**
+ * @author Robert Stein
+ *
+ */
+public class TestReportCreator {
+
+	
+	@Test
+	public void testgetGraphPanel(){
+		ReportCreator reporter = new ReportCreator();
+		//reporter.getGraphPanel(myProfile, myResultset);
+		
+	}
+
+}
