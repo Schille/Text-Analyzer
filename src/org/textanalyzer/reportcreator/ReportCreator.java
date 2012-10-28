6e0c3ddbea3d0ef91eaf46afbad3506aae47cbe8
Michael Schilonka <michael@schilonka.de>
2012-10-28 19:23:34 +0100

    added report creator class

---
+++ src/org/textanalyzer/reportcreator/ReportCreator.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.textanalyzer.reportcreator;
+
+import java.util.List;
+
+import javax.swing.JPanel;
+
+import org.textanalyzer.analyzer.IResultSet;
+import org.textanalyzer.database.IProfileInformation;
+
+/**
+ * @author Robert Stein
+ *
+ */
+public class ReportCreator implements IReportCreator {
+
+	/* (non-Javadoc)
+	 * @see org.textanalyzer.reportcreator.IReportCreator#getGraphPanel(org.textanalyzer.database.IProfileInformation, org.textanalyzer.analyzer.IResultSet)
+	 */
+	@Override
+	public JPanel getGraphPanel(IProfileInformation myProfile, IResultSet myResultset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.textanalyzer.reportcreator.IReportCreator#getGraphPanel(org.textanalyzer.database.IProfileInformation, java.util.List)
+	 */
+	@Override
+	public JPanel getGraphPanel(IProfileInformation myProfile, List<IResultSet> myResultset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
