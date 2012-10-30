28353346613b109c12d22ddcbd38551a233b6281
Michael Schilonka <michael@schilonka.de>
2012-10-30 16:09:51 +0100

    added DBDictionary

---
+++ src/org/textanalyzer/database/DBDictionary.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.textanalyzer.database;
+
+import org.textanalyzer.analyzer.dictionary.WordStatus;
+
+/**
+ * @author mschilonka
+ *
+ */
+public class DBDictionary implements IDBDictionary {
+
+	/* (non-Javadoc)
+	 * @see org.textanalyzer.database.IDBDictionary#getWordStatus(java.lang.String)
+	 */
+	@Override
+	public WordStatus getWordStatus(String myWord) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.textanalyzer.database.IDBDictionary#setWordStatus(java.lang.String, org.textanalyzer.analyzer.dictionary.WordStatus)
+	 */
+	@Override
+	public void setWordStatus(String myWord, WordStatus myStatus) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
