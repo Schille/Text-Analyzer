bf699ef142de37ba71bf2280df7dcb9bbc8ac4f9
Sebastian Hirschl <sebastian.hirschl@gmx.de>
2012-10-30 14:52:45 +0100

    added classpath and lib folder

---
+++ src/org/textanalyzer/documentimporter/Document.java
@@ -0,0 +1,11 @@
+package org.textanalyzer.documentimporter;
+
+public class Document implements IDocumentImporter {
+
+	@Override
+	public IDocument invokeNewDocumentImport() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
