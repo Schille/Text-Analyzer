2413767a83778479f6637e53dcf501211c8b435b
Robert Stein <roddirockz@googlemail.com>
2012-11-05 20:23:48 +0100

    implemented gui for profileviewer

--- src/org/textanalyzer/profileviewer/ProfileViewer.java
+++ src/org/textanalyzer/profileviewer/ProfileViewer.java
@@ -1,10 +1,16 @@
 /**
-*
-*/
+ *
+ */
 package org.textanalyzer.profileviewer;
 
+import java.awt.Font;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 import org.textanalyzer.database.DBHandle;
@@ -13,32 +19,92 @@ import org.textanalyzer.database.IProfileInformation;
 import org.textanalyzer.database.IResultSet;
 
 /**
-* @author Michael Schilonka & Robert Stein
-*
-*/
+ * @author Michael Schilonka & Robert Stein
+ * 
+ */
 public class ProfileViewer implements IProfileViewer {
 
-private long userID;
-private JPanel ground;
-private DatabaseConnector connector;
-private IProfileInformation profileInformation;
-private List<IResultSet> resultSets;
+	private long userID;
+	private JPanel ground;
+	private DatabaseConnector connector;
+	private IProfileInformation profileInformation;
+	private List<IResultSet> resultSets;
 
+	// -----------Constructor------------
+	public ProfileViewer(int myUserID) {
+		userID = myUserID;
+		connector = new DatabaseConnector();
+		profileInformation = connector.getProfileInformation(myUserID);
+		resultSets = connector.getAllResultSets(myUserID);
+	}
 
-//-----------Constructor------------
-public ProfileViewer(int myUserID){
-userID = myUserID;
-connector = new DatabaseConnector();
-profileInformation = connector.getProfileInformation(myUserID);
-resultSets = connector.getAllResultSets(myUserID);
-}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.textanalyzer.profileviewer.IProfileViewer#getProfileViewer()
+	 */
+	@Override
+	public JPanel getProfileViewer() {
+		JPanel ground = new JPanel();
+		JLabel headline = new JLabel();
+		JLabel author = new JLabel();
+		JLabel age = new JLabel();
+		JLabel profession = new JLabel();
+		JButton av_analyse = new JButton();
+		JButton new_analyse = new JButton();
+		JList texte = new JList();
+		ArrayList<String> dataname = new ArrayList<String>();
+		
+		
+		ground.setLayout(null);
+		ground.setSize(500,400);
+		
+		headline.setText("Profilübersicht");
+		headline.setFont(new Font("Dialog", 1 ,20));
+		headline.setLocation(180, 3);
+		headline.setSize(400, 35);
+		
+		author.setText("Name: "+profileInformation.getLastName()+", "+profileInformation.getFirstName());
+		author.setLocation(40, 50);
+		author.setSize(250,30);
+		
+		age.setText("Alter: "+profileInformation.getAge());
+		age.setLocation(40,70);
+		age.setSize(250, 30);
+		
+		profession.setText("Profession: "+profileInformation.getProfession());
+		profession.setLocation(40,90);
+		profession.setSize(250,30);
+		
+		new_analyse.setText("Neue Textanalyse");
+		new_analyse.setSize(200, 50);
+		new_analyse.setLocation(40,170);
+		new_analyse.setFocusPainted(false);
+		
+		av_analyse.setText("Durchschnittsanalyse starten");
+		av_analyse.setSize(200, 50);
+		av_analyse.setLocation(40,240);
+		av_analyse.setFocusPainted(false);
+		
+		texte.setSize(180, 300);
+		texte.setLocation(280, 50);
+		
+		Iterator<?> result = resultSets.iterator();
+		while(result.hasNext()) {
+			IResultSet temp_res = (IResultSet)result.next();
+			dataname.add("something");	
+		}
+		texte.setListData(dataname.toArray());
+		
+		ground.add(headline);
+		ground.add(author);
+		ground.add(age);
+		ground.add(profession);
+		ground.add(av_analyse);
+		ground.add(new_analyse);
+		ground.add(texte);
 
-/* (non-Javadoc)
-* @see org.textanalyzer.profileviewer.IProfileViewer#getProfileViewer()
-*/
-@Override
-public JPanel getProfileViewer() {
-return ground;
-}
+		return ground;
+	}
 
 }
---
+++ src/org/textanalyzer/tests/TestProfileViewer.java
@@ -0,0 +1,196 @@
+/**
+ * 
+ */
+package org.textanalyzer.tests;
+
+import static org.junit.Assert.*;
+
+import java.awt.Dimension;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.textanalyzer.database.DatabaseConnector;
+import org.textanalyzer.database.IResultSet;
+import org.textanalyzer.database.ProfileInformation;
+import org.textanalyzer.database.ResultSet;
+import org.textanalyzer.documentimporter.DocumentFormat;
+import org.textanalyzer.documentimporter.IDocument;
+import org.textanalyzer.profileviewer.ProfileViewer;
+
+/**
+ * @author Robert Stein
+ *
+ */
+public class TestProfileViewer {
+
+	DatabaseConnector test1;
+	ProfileInformation profile;
+	ResultSet result1;
+	ResultSet result;
+	ProfileInformation myProfile;
+	List<IResultSet> myResultSets;
+	long c;
+	long d;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		 test1 = new DatabaseConnector();
+
+		 profile = new ProfileInformation();
+		profile.setAge(99999);
+		profile.setFirstName("Peteasdasdr");
+		profile.setLastName("Klauasdasds");
+		profile.setProfession("Student");
+		HashMap<String, Integer> most = new HashMap<String, Integer>();
+		most.put("Haus", 20);
+		most.put("Garten", 15);
+		HashMap<String, Integer> custom = new HashMap<String, Integer>();
+		custom.put("Schlauberger", 5);
+		custom.put("Scheiße", 2);
+		 result = new ResultSet();
+		result.setAvaragePhraseLength(12);
+		result.setPseudoIQ(40);
+		result.setWordCount(500);
+		result.setWrongWordCount(50);
+		result.setMostFrequentWord(most);
+		result.setCustomWordCount(custom);
+		result.setDocument(new IDocument() {
+			
+			@Override
+			public String getText() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Date getImportDate() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String getFileName() {
+				// TODO Auto-generated method stub
+				return "fancyprogram";
+			}
+			
+			@Override
+			public String getDocumentPath() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public DocumentFormat getDocumentFormat() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});
+		 result1 = new ResultSet();
+		result1.setAvaragePhraseLength(42);
+		result1.setPseudoIQ(455);
+		result1.setWordCount(5500);
+		result1.setWrongWordCount(550);
+		result1.setMostFrequentWord(most);
+		result1.setCustomWordCount(custom);
+		result1.setDocument(new IDocument() {
+			
+			@Override
+			public String getText() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Date getImportDate() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String getFileName() {
+				// TODO Auto-generated method stub
+				return "cooltext";
+			}
+			
+			@Override
+			public String getDocumentPath() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public DocumentFormat getDocumentFormat() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});
+
+		assertEquals(1000,test1.saveProfileInformation(profile));
+		assertEquals(1001,test1.saveProfileInformation(profile));
+		test1.saveResultSet(1000,result);
+		test1.saveResultSet(1000, result1);
+		test1.saveResultSet(1001,result);
+		test1.saveResultSet(1001, result1);
+
+
+		c = test1.countClass("ProfileInformation");
+		d = test1.countClass("ResultSet");
+
+		assertEquals(2, c);
+		assertEquals(4, d);
+
+		myProfile = test1.getProfileInformation(1000);
+		myResultSets = test1.getAllResultSets(myProfile.getId());
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		test1.removeObject(myProfile);
+		test1.removeObject(myResultSets.get(0));
+		test1.removeObject(myResultSets.get(1));
+		
+		myProfile = test1.getProfileInformation(1001);
+		myResultSets = test1.getAllResultSets(myProfile.getId());
+		
+
+		
+		test1.removeObject(myProfile);
+		test1.removeObject(myResultSets.get(0));
+		test1.removeObject(myResultSets.get(1));
+
+
+		c = test1.countClass("ProfileInformation");
+		d = test1.countClass("ResultSet");
+
+		assertEquals(0, c);
+		assertEquals(0, d);
+		System.out.println("Deleted the stuff");
+	}
+
+	@Test
+	public void test() {
+		ProfileViewer viewer = new ProfileViewer(1000);
+		
+		  JFrame frame = new JFrame("Viewer");
+		  frame.setPreferredSize(new Dimension(500, 400));
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.getContentPane().add(viewer.getProfileViewer());
+	        frame.pack();
+	        frame.setVisible(true);
+
+	        frame.setAlwaysOnTop(true);
+	}
+
+}
