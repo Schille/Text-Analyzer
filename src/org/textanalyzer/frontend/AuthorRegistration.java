722d2fbcc59a7dedff62b551f83a1b2635a37818
Katharina Sandrock <katharina_sandrock@yahoo.de>
2012-11-08 11:03:19 +0100

    test

--- src/org/textanalyzer/frontend/AuthorRegistration.java
+++ src/org/textanalyzer/frontend/AuthorRegistration.java
@@ -14,6 +14,8 @@ public class AuthorRegistration extends JFrame {
     private javax.swing.JTextField profession;
     private javax.swing.JTextField age;
 
+    
+    
    
     public AuthorRegistration(){
     	
