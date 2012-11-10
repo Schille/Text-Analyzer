7663fbe0a875353079311db662c514b4f96f12fa
Katharina Sandrock <katharina_sandrock@yahoo.de>
2012-11-10 14:53:40 +0100

    First and last name

--- src/org/textanalyzer/frontend/AuthorRegistration.java
+++ src/org/textanalyzer/frontend/AuthorRegistration.java
@@ -20,11 +20,13 @@ import org.textanalyzer.profilemanager.ProfileManager;
 public class AuthorRegistration extends JPanel {
 
     private javax.swing.JLabel headLine = new JLabel("Anlegen eines neuen Autors");
-    private javax.swing.JLabel authornameLabel = new JLabel("Autorname: ");
+    private javax.swing.JLabel firstNameLabel = new JLabel("Vorname: ");
+    private javax.swing.JLabel lastNameLabel = new JLabel("Nachname: ");
     private javax.swing.JLabel professionLabel = new JLabel("Beruf: ");
     private javax.swing.JLabel ageLabel = new JLabel("Alter: ");
     private javax.swing.JButton submit = new JButton("Autor anlegen");
-    private javax.swing.JTextField authornameField = new JTextField();
+    private javax.swing.JTextField firstNameField = new JTextField();
+    private javax.swing.JTextField lastNameField = new JTextField();
     private javax.swing.JTextField professionField = new JTextField();
     private javax.swing.JTextField ageField = new JTextField();
     private ProfileManager manager;
@@ -43,21 +45,24 @@ public class AuthorRegistration extends JPanel {
     	headLine.setFont(new Font("Arial",0,24));
     	
     	//set the Labels that inform the users about the needed input
-    	authornameLabel.setBounds(100,100,93,22);
-    	authornameLabel.setFont(new Font("Arial",0,16));
-    	professionLabel.setBounds(100,150,93,22);
+    	firstNameLabel.setBounds(100,100,93,22);
+    	firstNameLabel.setFont(new Font("Arial",0,16));
+    	lastNameLabel.setBounds(100,150,93,22);
+    	lastNameLabel.setFont(new Font("Arial",0,16));
+    	professionLabel.setBounds(100,200,93,22);
     	professionLabel.setFont(new Font("Arial",0,16));
-    	ageLabel.setBounds(100,200,93,22);
+    	ageLabel.setBounds(100,250,93,22);
     	ageLabel.setFont(new Font("Arial",0,16));
     	
     	
     	//set the three text fields
-    	authornameField.setBounds(200,100,190,25);
-    	professionField.setBounds(200,150,190,25);
-    	ageField.setBounds(200,200,190,25);
+    	firstNameField.setBounds(200,100,190,25);
+    	lastNameField.setBounds(200,150,190,25);
+    	professionField.setBounds(200,200,190,25);
+    	ageField.setBounds(200,250,190,25);
     	
     	//set the Button to submit the input
-    	submit.setBounds(175,270,130,30);
+    	submit.setBounds(200,300,130,30);
     	submit.setForeground(Color.white);
     	submit.setBackground(Color.red);
     	submit.setFont(new Font("Arial",0,15));
@@ -73,10 +78,12 @@ public class AuthorRegistration extends JPanel {
 		
 	
 		add(headLine);
-		add(authornameField);
+		add(firstNameField);
+		add(lastNameField);
 		add(professionField);
 		add(ageField);
-    	add(authornameLabel);
+    	add(firstNameLabel);
+    	add(lastNameLabel);
     	add(professionLabel);
     	add(ageLabel);
     	add(submit);
@@ -91,17 +98,27 @@ public class AuthorRegistration extends JPanel {
 	protected void submitButtonActionPerformed(ActionEvent evt) {
 		
 		ProfileInformation myProfileInfo = new ProfileInformation();
-		String author = authornameField.getText();
+		String authorFirstName = firstNameField.getText();
+		String authorLastName = lastNameField.getText();
 		String profession = professionField.getText();
 		
-		validateString(author);
-		if (validateString(author)){
-			myProfileInfo.setFirstName("Klausi");
-			myProfileInfo.setLastName("Peterson");
+		
+		if (validateString(authorFirstName)){
+			myProfileInfo.setFirstName(authorFirstName);
+	
 			
 		} else {
-			JOptionPane.showMessageDialog(null, "Bitte geben Sie einen Autorennamen an. Achten Sie bitte dabei darauf, dass er nur Buchstaben enth\u00e4lt und keine Zahlen oder Sonderzeichen.", "Fehler", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Bitte geben Sie den Vornamen des Autors an. Achten Sie bitte dabei darauf, dass er nur Buchstaben enth\u00e4lt und keine Zahlen oder Sonderzeichen.", "Fehler", JOptionPane.ERROR_MESSAGE);
 		}
+		
+		if (validateString(authorLastName)){
+			myProfileInfo.setLastName(authorLastName);
+			
+		} else {
+			JOptionPane.showMessageDialog(null, "Bitte geben Sie den Nachnamen des Autors an. Achten Sie bitte dabei darauf, dass er nur Buchstaben enth\u00e4lt und keine Zahlen oder Sonderzeichen.", "Fehler", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		
 		
 		if (validateString(profession)){
 			myProfileInfo.setProfession(profession);
