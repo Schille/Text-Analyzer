613630233628ee4904710be7c9fcbf59725e3919
Maximilian Quellmalz <max.quellmalz@freenet.de>
2012-11-13 09:31:04 +0100

    Fixed id assignment

--- src/org/textanalyzer/database/DatabaseConnector.java
+++ src/org/textanalyzer/database/DatabaseConnector.java
@@ -4,123 +4,136 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
-
-
 /**
  * Database Connector
+ * 
  * @author Michael Schilonka
  * @author Maximilian Quellmalz
  * @version 12.11.2012
  */
 
-public class DatabaseConnector implements IDatabaseConnector{
-	
+public class DatabaseConnector implements IDatabaseConnector {
+
 	private DBHandle connector;
-	
+
 	/**
 	 * Creates new instance of database.
-	 * @param connector new instance of database
+	 * 
+	 * @param connector
+	 *            new instance of database
 	 */
-	public DatabaseConnector(){
+	public DatabaseConnector() {
 		connector = DBHandle.createDB();
 	}
-	
+
 	/**
-	 * Overrides method removeObject from IDatabaseConnector.
-	 * Removes a given object from database.
+	 * Overrides method removeObject from IDatabaseConnector. Removes a given
+	 * object from database.
+	 * 
 	 * @see IDatabaseConnector#removeObject(Object)
 	 */
 	@Override
 	public void removeObject(Object myObject) {
 		connector.delete(myObject);
 	}
-	
+
 	/**
-	 * Removes a given profile (ProfileInformation & ResultSets) by a given Id from database.
+	 * Removes a given profile (ProfileInformation & ResultSets) by a given Id
+	 * from database.
 	 */
-	public void removeProfile(long myID){
+	public void removeProfile(long myID) {
 		ProfileInformation tobedeleted = getProfileInformation((int) myID);
 		removeObject(tobedeleted);
-		for (ResultSet p : connector.browseClass(ResultSet.class)){
-			if (p.getId()==myID) {
+		for (ResultSet p : connector.browseClass(ResultSet.class)) {
+			if (p.getId() == myID) {
 				removeObject(p);
 				removeObject(p.getDocument());
-			}	
+			}
 		}
 	}
 
 	/**
-	 * Overrides function getAllProfiles from IDatabaseConnector.
-	 * Returns a List of all existing profiles in the database.
+	 * Overrides function getAllProfiles from IDatabaseConnector. Returns a List
+	 * of all existing profiles in the database.
+	 * 
 	 * @see IDatabaseConnector#getAllProfiles()
 	 * @return list of all existing profiles
 	 */
 	@Override
 	public List<ProfileInformation> getAllProfiles() {
-		 List<ProfileInformation> profileList = new LinkedList<ProfileInformation>();
-		 for (ProfileInformation p : connector.browseClass(ProfileInformation.class)){
-		 profileList.add(p);
-		 }
-		 return profileList;
+		List<ProfileInformation> profileList = new LinkedList<ProfileInformation>();
+		for (ProfileInformation p : connector
+				.browseClass(ProfileInformation.class)) {
+			profileList.add(p);
+		}
+		return profileList;
 	}
 
 	/**
-	 * Overrides function getProfileInformation from IDatabaseConnector.
-	 * Returns a an existing profile in the database by id.
+	 * Overrides function getProfileInformation from IDatabaseConnector. Returns
+	 * a an existing profile in the database by id.
+	 * 
 	 * @see IDatabaseConnector#getProfileInformation(int)
 	 * @return profile by id
 	 */
 	@Override
 	public ProfileInformation getProfileInformation(int myId) {
 		ProfileInformation help = null;
-		for (ProfileInformation p : connector.browseClass(ProfileInformation.class)){
-			if (p.getId()==myId) {
-				help=p;
-			}				
+		for (ProfileInformation p : connector
+				.browseClass(ProfileInformation.class)) {
+			if (p.getId() == myId) {
+				help = p;
+			}
 		}
 		return help;
 	}
 
 	/**
-	 * Overrides method editProfile from IDatabaseConnector.
-	 * Assigns new profile information to a given object of ProfileInformation by id.
+	 * Overrides method editProfile from IDatabaseConnector. Assigns new profile
+	 * information to a given object of ProfileInformation by id.
+	 * 
 	 * @see IDatabaseConnector#editProfile(int, ProfileInformation)
 	 */
 	@Override
 	public void editProfile(int myId, ProfileInformation myProfile) {
-		for (ProfileInformation p : connector.browseClass(ProfileInformation.class)){
-			if (p.getId()==myId) {
+		for (ProfileInformation p : connector
+				.browseClass(ProfileInformation.class)) {
+			if (p.getId() == myId) {
 				p = myProfile;
 				connector.save(p);
-			}			
+			}
 		}
-		
+
 	}
-	
+
 	/**
-	 * Help function.
-	 * Counts instances of a given class name in the database.
+	 * Help function. Counts instances of a given class name in the database.
+	 * 
 	 * @return number of instances of given class
 	 */
-	public long countClass(String myClassName){
+	public long countClass(String myClassName) {
 		return connector.countClass(myClassName);
 	}
 
 	/**
-	 * Overrides function getAllResultSets from IDatabaseConnector.
-	 * Returns a List of all existing ResultSets by id in the database.
+	 * Overrides function getAllResultSets from IDatabaseConnector. Returns a
+	 * List of all existing ResultSets by id in the database.
+	 * 
 	 * @see IDatabaseConnector#getAllResultSets(long)
 	 * @return list of all existing result sets for a given id
 	 */
 	@Override
 	public List<IResultSet> getAllResultSets(long myId) {
-	    List<IResultSet> result = new LinkedList<IResultSet>();
-	    for (ResultSet p : connector.browseClass(ResultSet.class)){
-			if(p.getId() == myId){
-				//result.add((IResultSet) connector.getUserObjectByRecord((OIdentifiable) p, "ResultSet"));
+		List<IResultSet> result = new LinkedList<IResultSet>();
+		for (ResultSet p : connector.browseClass(ResultSet.class)) {
+			if (p.getId() == myId) {
+				// result.add((IResultSet)
+				// connector.getUserObjectByRecord((OIdentifiable) p,
+				// "ResultSet"));
 				ResultSet tmp = new ResultSet();
 				tmp.setAvaragePhraseLength(p.getAvaragePhraseLength());
-				tmp.setCustomWordCount((HashMap<String, Integer>) p.getCustomWordCount());
+				tmp.setCustomWordCount((HashMap<String, Integer>) p
+						.getCustomWordCount());
 				tmp.setDocument(p.getDocument());
 				tmp.setId(p.getId());
 				tmp.setMostFrequentNomen(p.getMostFrequentNomen());
@@ -136,27 +149,41 @@ public class DatabaseConnector implements IDatabaseConnector{
 	}
 
 	/**
-	 * Overrides function saveProfileInformation from IDatabaseConnector.
-	 * Saves new profile information object in the database.
+	 * Overrides function saveProfileInformation from IDatabaseConnector. Saves
+	 * new profile information object in the database.
+	 * 
 	 * @see IDatabaseConnector#saveProfileInformation(IProfileInformation)
 	 * @return id of saved object
 	 */
 	@Override
 	public long saveProfileInformation(IProfileInformation myObject) {
-		long id = connector.countClass("ProfileInformation") + 1000;
-		myObject.setId(id);
-		connector.save(myObject);		
-		return id;
+		long helpid = 1000;
+
+		if ((connector.countClass("ProfileInformation")) == 0) {
+			helpid = 999;
+		} else {
+			for (ProfileInformation p : connector
+					.browseClass(ProfileInformation.class)) {
+				if (helpid < p.getId()) {
+					helpid = p.getId();
+				}
+			}
+		}
+		myObject.setId(helpid + 1);
+		System.out.println(helpid + 1);
+		connector.save(myObject);
+		return helpid + 1;
 	}
 
 	/**
-	 * Overrides function saveResultSet from IDatabaseConnector.
-	 * Saves new result set object in the database.
+	 * Overrides function saveResultSet from IDatabaseConnector. Saves new
+	 * result set object in the database.
+	 * 
 	 * @see IDatabaseConnector#saveResultSet(int, IResultSet)
 	 */
 	@Override
 	public void saveResultSet(int myProfileID, IResultSet myObject) {
-		((ResultSet)myObject).setId(myProfileID);
-		connector.save(myObject);				
+		((ResultSet) myObject).setId(myProfileID);
+		connector.save(myObject);
 	}
 }
