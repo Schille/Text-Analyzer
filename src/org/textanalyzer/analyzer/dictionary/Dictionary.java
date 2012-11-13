93cb9b80896e6b3f8b6d29c26e48e88d06e69605
Fabian Brabaender <fabian@brabaender-kriegsfeld.de>
2012-11-13 11:16:35 +0100

    More comments in the dictionary class

--- src/org/textanalyzer/analyzer/dictionary/Dictionary.java
+++ src/org/textanalyzer/analyzer/dictionary/Dictionary.java
@@ -18,109 +18,161 @@ import org.textanalyzer.database.DBDictionary;
  */
 
 public class Dictionary implements IDictionary {
-	
+
 	/**
-	 * This class checks the correctness of a given word. After the check, the result is written into the database
-	 * to process faster in the future
-	 * @param String Word
+	 * This class checks the correctness of a given word. After the check, the
+	 * result is written into the database to process faster in the future
+	 * 
+	 * @param String
+	 *            Word
 	 * @return WordStatus
 	 */
 
 	DBDictionary db;
-	int i=0;
+	int i = 0;
+
 	public Dictionary() {
 		db = new DBDictionary();
 	}
 
 	@SuppressWarnings("resource")
 	public WordStatus dudenRequest(String myRequest) throws IOException {
-		
+
 		/**
-		 * This method checks the word in the online dictionary "Duden.de" 
+		 * This method checks the word in the online dictionary "Duden.de"
 		 */
-		
+
 		WordStatus result;
 		String requestString;
 		try {
-			requestString = URLDecoder.decode("http://www.duden.de/rechtschreibung/" + myRequest, "UTF-8");
+			/**
+			 * URL to access the dictionary
+			 */
+			requestString = URLDecoder
+					.decode("http://www.duden.de/rechtschreibung/" + myRequest,
+							"UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			requestString = "http://www.duden.de/rechtschreibung/" + myRequest;
 		}
 
 		URL url = new URL(requestString);
+		/**
+		 * The data of the website is written into the input stream, after that
+		 * scanner checks which information the site contains about the
+		 * word(verb, noun etc.)
+		 */
 		InputStream stream = url.openStream();
 		String dudenResponse = new Scanner(stream).useDelimiter("\\A").next();
-		
+
 		if (dudenResponse.contains("<span class=\"wortart\">Adjektiv"))
 			result = WordStatus.ADJECTIV;
-		else if (dudenResponse.contains("<div class=\"field-item even\">Adjektiv"))
+		else if (dudenResponse
+				.contains("<div class=\"field-item even\">Adjektiv"))
 			result = WordStatus.ADJECTIV;
 		else if (dudenResponse.contains("<span class=\"wortart\">Substantiv"))
 			result = WordStatus.NOMEN;
-		else if (dudenResponse.contains("<div class=\"field-item even\">Substantiv"))
+		else if (dudenResponse
+				.contains("<div class=\"field-item even\">Substantiv"))
 			result = WordStatus.NOMEN;
-		else if (dudenResponse.contains("<div class=\"field-item even\">schwaches Verb"))
+		else if (dudenResponse
+				.contains("<div class=\"field-item even\">schwaches Verb"))
 			result = WordStatus.VERB;
-		else if (dudenResponse.contains("<span class=\"wortart\">schwaches Verb"))
+		else if (dudenResponse
+				.contains("<span class=\"wortart\">schwaches Verb"))
 			result = WordStatus.VERB;
-		else if (dudenResponse.contains("<div class=\"field-item even\">starkes Verb"))
+		else if (dudenResponse
+				.contains("<div class=\"field-item even\">starkes Verb"))
 			result = WordStatus.VERB;
 		else if (dudenResponse.contains("<span class=\"wortart\">starkes Verb"))
 			result = WordStatus.VERB;
 		else
 			result = WordStatus.OTHER;
-
-			db.setWordStatus(myRequest, result); // Set WordStatus in local Database
-			return result;
-	}
-	
-	public WordStatus freeDictionaryRequest(String myRequest) throws Exception {
-		
 		/**
-		 * This method checks the word in the online dictionary "the free dictionary"
+		 * The result of the request is written into the database and the
+		 * WordStatus is returned
 		 */
-		
+		db.setWordStatus(myRequest, result); // Set WordStatus in local Database
+		return result;
+	}
+
+	public WordStatus freeDictionaryRequest(String myRequest) throws Exception {
+
+		/**
+		 * This method checks the word in the online dictionary
+		 * "the free dictionary"
+		 */
+
 		String requestString;
 		try {
-			requestString = URLDecoder.decode("http://de.thefreedictionary.com/" + myRequest, "UTF-8");
+			/**
+			 * URL to access the dictionary
+			 */
+			requestString = URLDecoder.decode(
+					"http://de.thefreedictionary.com/" + myRequest, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			requestString = "http://de.thefreedictionary.com/" + myRequest;
 		}
 
 		URL url = new URL(requestString);
 		URLConnection connection = url.openConnection();
-		connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+		/**
+		 * The user-agent is set to a specific browser so the content is
+		 * accessible. If this won't happen the website recognize that this
+		 * request is opened by other software and so the access is reclined
+		 */
+		connection
+				.setRequestProperty(
+						"User-Agent",
+						"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+		/**
+		 * The data of the website is written into the input stream, after that
+		 * scanner checks which information the site contains about the
+		 * word(verb, noun etc.)
+		 */
 		InputStream stream = connection.getInputStream();
 		String freeResponse = new Scanner(stream).useDelimiter("\\A").next();
-		
-		freeResponse = freeResponse.replace("/"+myRequest, " ");
-		freeResponse = freeResponse.replace(">"+myRequest, " ");
-		if(freeResponse.contains("Das Wort konnte im Wörterbuch nicht gefunden werden.")){
+
+		freeResponse = freeResponse.replace("/" + myRequest, " ");
+		freeResponse = freeResponse.replace(">" + myRequest, " ");
+		if (freeResponse
+				.contains("Das Wort konnte im Wörterbuch nicht gefunden werden.")) {
 			stream.close();
 			throw new Exception("Word not found!");
 		}
-			
-		db.setWordStatus(myRequest, WordStatus.OTHER); // Set WordStatus in local Database
+
+		db.setWordStatus(myRequest, WordStatus.OTHER); // Set WordStatus in
+														// local Database
 		return WordStatus.OTHER;
 	}
-	
-	private WordStatus wiktionaryRequest(String myRequest) throws Exception{
-		
+
+	private WordStatus wiktionaryRequest(String myRequest) throws Exception {
+
 		/**
-		 * This method checks the word in the online dictionary "Wiktionary" 
+		 * This method checks the word in the online dictionary "Wiktionary"
 		 */
 		WordStatus result;
 		String requestString;
 		try {
-			requestString = URLDecoder.decode("http://de.wiktionary.org/w/api.php?action=query&prop=categories&format=xml&titles=" + myRequest, "UTF-8");
+			/**
+			 * URL to access the dictionary
+			 */
+			requestString = URLDecoder
+					.decode("http://de.wiktionary.org/w/api.php?action=query&prop=categories&format=xml&titles="
+							+ myRequest, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
-			requestString = "http://de.wiktionary.org/w/api.php?action=query&prop=categories&format=xml&titles=" + myRequest;
+			requestString = "http://de.wiktionary.org/w/api.php?action=query&prop=categories&format=xml&titles="
+					+ myRequest;
 		}
-		
+
 		URL url_wiktionary = new URL(requestString);
+		/**
+		 * The data of the website is written into the input stream, after that
+		 * scanner checks which information the site contains about the
+		 * word(verb, noun etc.)
+		 */
 		InputStream wikiStream = url_wiktionary.openStream();
 		String wikiResponse = new Scanner(wikiStream).useDelimiter("\\A")
-					.next();
+				.next();
 
 		if (wikiResponse.contains("Adjektiv"))
 			result = WordStatus.ADJECTIV;
@@ -130,28 +182,28 @@ public class Dictionary implements IDictionary {
 			result = WordStatus.OTHER;
 		else if (wikiResponse.contains("Substantiv"))
 			result = WordStatus.NOMEN;
-		else if (wikiResponse.contains("Verb")||wikiResponse.contains("Konjugierte Form"))
+		else if (wikiResponse.contains("Verb")
+				|| wikiResponse.contains("Konjugierte Form"))
 			result = WordStatus.VERB;
 		else if (wikiResponse.contains("Kategorie:Deutsch"))
 			result = WordStatus.OTHER;
-		else if (wikiResponse.contains("missing\" \"")){
+		else if (wikiResponse.contains("missing\" \"")) {
+			wikiStream.close();
+			throw new Exception("Word not found!");
+		} else {
 			wikiStream.close();
 			throw new Exception("Word not found!");
 		}
-		else{
-			wikiStream.close();
-			throw new Exception("Word not found!");
-		}
-		
+
 		db.setWordStatus(myRequest, result);
 		return result;
 	}
-	
-	private String revertParticiple1(String myWord){
+
+	private String revertParticiple1(String myWord) {
 		return myWord.substring(0, myWord.length() - 1);
 	}
-	
-	private String revertParticiple2(String myWord){
+
+	private String revertParticiple2(String myWord) {
 		return myWord.substring(2, myWord.length() - 1) + "en";
 	}
 
@@ -166,29 +218,33 @@ public class Dictionary implements IDictionary {
 	@SuppressWarnings("resource")
 	@Override
 	public WordStatus getWordStatus(String myWord) {
-
+		/**
+		 * This method starts the process with calling checkWord. If the result
+		 * is wrong the word is checked again if it is a participle 1/2 and if
+		 * so it's checked again
+		 */
 		WordStatus result = WordStatus.OTHER;
-		//check the word status
+		// check the word status
 		result = checkWord(myWord);
-		
-		//Just check other german word classes
-		if(result == WordStatus.WRONG){
-			
-			//Check whether the word is in participle I
+
+		// Just check other german word classes
+		if (result == WordStatus.WRONG) {
+
+			// Check whether the word is in participle I
 			result = checkWord(revertParticiple1(myWord));
-			if(result != WordStatus.WRONG){
+			if (result != WordStatus.WRONG) {
 				return result;
 			}
-			
-			//Check whether the word is in participle II
+
+			// Check whether the word is in participle II
 			String a = revertParticiple2(myWord);
 			result = checkWord(a);
-			if(result != WordStatus.WRONG){
+			if (result != WordStatus.WRONG) {
 				return result;
 			}
-			
+
 		}
-		
+
 		return result;
 	}
 
@@ -197,34 +253,52 @@ public class Dictionary implements IDictionary {
 	 * @return
 	 */
 	private WordStatus checkWord(String myWord) {
-		System.out.println("\nLook up: " +myWord);
+		/**
+		 * This method check if the word already exists in the locale database,
+		 * if not, different dictionarys are requested.
+		 */
+		System.out.println("\nLook up: " + myWord);
+		/**
+		 * lookup the word in locale database
+		 */
 		WordStatus dbWordStatus = db.getWordStatus(myWord);
-		
-		if(dbWordStatus != null){
-			System.out.println("DB: " + myWord + " | " + dbWordStatus.toString());
+
+		// If word exists in database, it's returned
+		if (dbWordStatus != null) {
+			System.out.println("DB: " + myWord + " | "
+					+ dbWordStatus.toString());
 			return dbWordStatus;
 		}
-			
-		try{
+
+		try {
+			// Check the word in the dictionary "wiktionary"
 			dbWordStatus = wiktionaryRequest(myWord);
-			System.out.println("Wiktionary: "+ myWord + " | " + dbWordStatus.toString());
+			System.out.println("Wiktionary: " + myWord + " | "
+					+ dbWordStatus.toString());
 			return dbWordStatus;
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
+			try {
+				// If the wiktionary request throws a exception(WordStatus could
+				// not be resolved, the Duden request is executed
+				dbWordStatus = dudenRequest(myWord);
+				System.out.println("Duden: " + myWord + " | "
+						+ dbWordStatus.toString());
+				return dbWordStatus;
+			} catch (IOException e1) {
+				// If the Duden request throws a exception(WordStatus could not
+				// be resolved, the free dictonary request is executed
 				try {
-					dbWordStatus = dudenRequest(myWord);
-					System.out.println("Duden: "+ myWord + " | " + dbWordStatus.toString());
+					dbWordStatus = freeDictionaryRequest(myWord);
+					System.out.println("FreeDictionary: " + myWord + " | "
+							+ dbWordStatus.toString());
 					return dbWordStatus;
-				} catch (IOException e1) {
-					try {
-						dbWordStatus = freeDictionaryRequest(myWord);
-						System.out.println("FreeDictionary: "+ myWord + " | " + dbWordStatus.toString());
-						return dbWordStatus;
-					} catch (Exception e2) {
-						System.out.println("WRONG: "+ myWord + " | " + WordStatus.WRONG.toString());
-						return WordStatus.WRONG;
-					}
+				} catch (Exception e2) {
+					System.out.println("WRONG: " + myWord + " | "
+							+ WordStatus.WRONG.toString());
+					return WordStatus.WRONG;
 				}
+			}
 		}
 	}
 }
