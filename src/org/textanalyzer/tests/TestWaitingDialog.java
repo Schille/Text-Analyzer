ef0a39191923d3984e619f1e42484772fd8aa3c4
Robert Stein <roddirockz@googlemail.com>
2012-11-20 11:29:33 +0100

    Update src/org/textanalyzer/tests/TestWaitingDialog.java
    added author

--- src/org/textanalyzer/tests/TestWaitingDialog.java
+++ src/org/textanalyzer/tests/TestWaitingDialog.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import org.textanalyzer.frontend.WaitingDialog;
 
 public class TestWaitingDialog {
-
+/**
+* @author Robert Stein
+*/
 	@Test
 	public void test() {
 		WaitingDialog testdia = new WaitingDialog();
